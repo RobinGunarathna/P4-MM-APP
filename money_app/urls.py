fff1a513d3d36da4cb880a1859d368e2cb0da141
RobinGunarathna <madgunrob@gmail.com>
2022-07-11 18:53:13 +0000

    missed comma in urls.py and small M in views instead of big now runserver works again

--- money_app/urls.py
+++ money_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name = "money_app")
-    path('add-money_app', views.add_mapp, name = 'add-money_app')
+    path('',views.index,name = "money_app"),
+    path('add-mapp', views.add_mapp, name ='add-money_app'),
 ]
