0f0dee437423ae98c0e4af411ab957e957e4dee8
RobinGunarathna <madgunrob@gmail.com>
2022-07-11 17:38:16 +0000

    adding more html and repeating steps for rendering

--- money_app/urls.py
+++ money_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('',views.index,name = "money_app")
+    path('add-money_app', views.add_mapp, name = 'add-money_app')
 ]
--- money_app/views.py
+++ money_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'templates/index.html')
+
+def add_mapp(request):
+    return render(request, 'templates/add_mapp.html')
