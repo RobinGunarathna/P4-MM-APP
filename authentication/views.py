ee9e58c21764634dff2bba37e637405d24585467
RobinGunarathna <madgunrob@gmail.com>
2022-07-12 22:06:08 +0000

    trying to get the post method working new functions and logic in register.js a

--- authentication/views.py
+++ authentication/views.py
@@ -14,11 +14,10 @@ class UsernameValidationView(View):
         data = json.loads(request.body)
         username = data['username']
         if not str(username).isalnum():
-            return JsonResponse({"Username should only contain alphanumeric characters (A to Z and 0 to 9"}, status = 400)
+            return JsonResponse({'username_error': 'username should only have alphanumeric characters Aa-Zz 0-9'}, status=400)
         if User.objects.filter(username=username).exists():
-            return JsonResponse({"Username is allready in use"}, status = 409)
-        
-        return JsonResponse({'username_valid' : True})
+            return JsonResponse({'username_error': 'sorry username allready in use, chose another one '}, status=409)
+        return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
--- mm_app/static/js/register.js
+++ mm_app/static/js/register.js
@@ -1,1 +1,50 @@
-console.log("register,working")
+const usernameField = document.querySelector('#usernameField');
+const feedBackArea = document.querySelector('.invalid-feedback')
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+
+usernameField.addEventListener('keyup', (e) => {
+    console.log(3333);
+    const usernameVal = e.target.value;
+
+    usernameField.classList.remove("is-invalid");
+    feedBackArea.style.display = 'none';
+
+    if (usernameVal.length > 0) {
+        fetch('/authentication/validate-username', {
+            method: 'POST',
+            mode: 'same-origin',
+            headers: {
+                'X-CSRFToken': csrftoken,
+                'X-Requested-With': 'XMLHttpRequest',
+                "Content-type": "application/json; charset=UTF-8",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({username: usernameVal}),
+        }).then((res) => res.json()).then((data) => {
+            console.log('data', data);
+            if (data.username_error) {
+                usernameField.classList.add("is-invalid");
+                feedBackArea.style.display = 'block';
+                feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
+            }
+        });
+    }
+});
