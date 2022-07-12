411b5dfd6ee6477151c387755b990d92e4ceff61
RobinGunarathna <madgunrob@gmail.com>
2022-07-12 16:17:18 +0000

    setting up website registration

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
 import json
 from django.http import JsonResponse
 
@@ -12,10 +13,12 @@ class UsernameValidationView(View):
     def post(self, request):
         data = json.loads(request.body)
         username = data['username']
-
-        if str(username).isalnum():
-            return JsonResponse({"Username should only contain alphanumeric characters (A to Z and 0 to 9.)"})
-        return JsonResponse({'username_valid', True})
+        if not str(username).isalnum():
+            return JsonResponse({"Username should only contain alphanumeric characters (A to Z and 0 to 9"}, status = 400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"Username is allready in use"}, status = 409)
+        
+        return JsonResponse({'username_valid' : True})
 
 class RegistrationView(View):
     def get(self, request):
